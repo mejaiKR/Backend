4a002f335c16bf4593ad428f1ba41877931527c5
hawardShin <joushin@student.42seoul.kr>
2024-02-15 15:16:15 +0900

    init Project

---
+++ src/main/java/mejai/mejaigg/MejaiGgApplication.java
@@ -0,0 +1,13 @@
+package mejai.mejaigg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MejaiGgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MejaiGgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mejai/mejaigg/MejaiGgApplicationTests.java
@@ -0,0 +1,13 @@
+package mejai.mejaigg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MejaiGgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
