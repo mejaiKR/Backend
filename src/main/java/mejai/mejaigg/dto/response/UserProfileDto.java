a3ef6b2ab0ee2d0a7414f9e9af65d71fdd3f68d7
hawardShin <joushin@student.42seoul.kr>
2024-03-16 21:10:39 +0900

    Fix: Fit For Dto

--- src/main/java/mejai/mejaigg/dto/response/UserProfileDto.java
+++ src/main/java/mejai/mejaigg/dto/response/UserProfileDto.java
@@ -2,15 +2,28 @@ package mejai.mejaigg.dto.response;
 
 import lombok.Getter;
 import lombok.Setter;
+import mejai.mejaigg.domain.Rank;
+import mejai.mejaigg.domain.User;
 
 @Getter
 @Setter
 public class UserProfileDto {
 	private String userId;
-	private String profileIcon;
+	private int profileIcon;
 	private String tier;
-	private String rank;
-	private int leaguePoints;
+	private int rank;
+	private Long leaguePoints;
 	private int wins;
 	private int losses;
+
+	public void setByUser(User user){
+		Rank rank = user.getRanks().stream().findFirst().orElse(null);
+		this.userId = user.getPuuid();
+		this.profileIcon = user.getProfileIconId();
+		this.tier = rank.getTier();
+		this.rank = rank.getRank();
+		this.leaguePoints = rank.getLeaguePoints();
+		this.wins = rank.getWins();
+		this.losses = rank.getLosses();
+	}
 }
