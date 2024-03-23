d03cce5294564d0e708c5b9924a2b5f955a1212f
hawardShin <joushin@student.42seoul.kr>
2024-03-23 17:07:35 +0900

    Feat: User Service with mapper class And Set optional

--- src/main/java/mejai/mejaigg/service/UserService.java
+++ src/main/java/mejai/mejaigg/service/UserService.java
@@ -1,23 +1,36 @@
 package mejai.mejaigg.service;
-import mejai.mejaigg.domain.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import lombok.RequiredArgsConstructor;
+import mejai.mejaigg.domain.Game;
+import mejai.mejaigg.domain.MatchParticipant;
+import mejai.mejaigg.domain.Rank;
+import mejai.mejaigg.domain.User;
+import mejai.mejaigg.domain.UserGameStat;
+import mejai.mejaigg.dto.response.UserProfileDto;
 import mejai.mejaigg.dto.riot.AccountDto;
 import mejai.mejaigg.dto.riot.RankDto;
 import mejai.mejaigg.dto.riot.SummonerDto;
 import mejai.mejaigg.dto.riot.match.MatchDto;
 import mejai.mejaigg.dto.riot.match.ParticipantDto;
-import mejai.mejaigg.repository.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import lombok.RequiredArgsConstructor;
-import mejai.mejaigg.dto.response.UserProfileDto;
-import org.springframework.web.server.ResponseStatusException;
+import mejai.mejaigg.mapper.GameMapper;
+import mejai.mejaigg.mapper.RankMapper;
+import mejai.mejaigg.mapper.UserMapper;
+import mejai.mejaigg.repository.GameRepository;
+import mejai.mejaigg.repository.RankRepository;
+import mejai.mejaigg.repository.UserGameStatRepository;
+import mejai.mejaigg.repository.UserRepository;
 import reactor.core.publisher.Mono;
-import org.springframework.transaction.annotation.Transactional;
-import reactor.core.publisher.Mono;
-
-import java.util.Set;
 
 @Service
 @Transactional(readOnly = true)
@@ -28,66 +41,76 @@ public class UserService {
 	private final UserRepository userRepository;
 	private final GameRepository gameRepository;
 	private final UserGameStatRepository userGameStatRepository;
+	private final RankRepository rankRepository;
 
 	@Value("${season:14}") // 기본값으로 14을 사용
 	private int season;
 	@Value("${resourceURL:https://ddragon.leagueoflegends.com/cdn/11.16.1/img/profileicon/}")
 	private String requestUrl;
+
 	//TODO : api 콜을 실패하는 경우 고려해야함
 	//TODO : 비동기 레포지토리 방식 적용
 	//TODO : 시즌 바꼈을때 추가하는 로직 필요하다.
 	//처음으로 요청이 들어왔을 때 호출되는 서비스
 	@Transactional(readOnly = false)
-	public String setUserProfile(String name,String tag){
-		User user = new User();
+	public String setUserProfile(String name, String tag) {
 		Mono<AccountDto> account = apiService.getAccountByNameAndTag(name, tag);
-		try{
-			AccountDto accountDto = account.block();
-			user.setByAccountDto(accountDto);
-			Mono<SummonerDto> summoner = apiService.getSummonerByPuuid(accountDto.getPuuid());
-			SummonerDto summonerDto = summoner.block();
-			user.setBySummonerDto(summonerDto);
-			Mono<Set<RankDto>> rankBySummonerId = apiService.getRankBySummonerId(summonerDto.getId());
-			Set<RankDto> rankDtos = rankBySummonerId.block();
-			RankDto firstElement = rankDtos.stream().findFirst().orElse(null);
-			Rank rank = new Rank();
-			rank.setRankByRankDto(firstElement,season);
-			user.addRank(rank);
-			userRepository.save(user);
-			Mono<String[]> matchHistoryByPuuid = apiService.getMatchHistoryByPuuid(accountDto.getPuuid(), 20);
+		AccountDto accountDto = account.block();
+		Mono<SummonerDto> summoner = apiService.getSummonerByPuuid(accountDto.getPuuid());
+		SummonerDto summonerDto = summoner.block();
+		User user = UserMapper.INSTANCE.toUserEntity(accountDto, summonerDto);
+
+		Mono<Set<RankDto>> rankBySummonerId = apiService.getRankBySummonerId(summonerDto.getId());
+		RankDto rankDto = rankBySummonerId.block()
+			.stream()
+			.findFirst()
+			.orElse(null);
+		Rank rank = new Rank();
+
+		if (rankDto != null)  //랭크가 없는 경우에는 배열이 비었다. (언랭 유저)
+			rank = RankMapper.INSTANCE.toRankEntity(rankDto);
+		else
+			rank.setUnRanked();
+		user.setRank(rank);
+		rankRepository.save(rank);
+		userRepository.save(user);
+		try {
+			Mono<String[]> matchHistoryByPuuid = apiService.getMatchHistoryByPuuid(accountDto.getPuuid(),
+				20); //임시로 20개임
 			String[] matchHistory = matchHistoryByPuuid.block();
 			for (String matchId : matchHistory) {
-				Game game = new Game();
 				MatchParticipant matchParticipant = new MatchParticipant();
 				Mono<MatchDto> gameInfo = apiService.getMatchDtoByMatchId(matchId);
 				MatchDto matchDto = gameInfo.block();
 
-				game.setByMatchDto(matchDto);
+				Game game = GameMapper.INSTANCE.toGameEntity(matchDto.getInfo(), matchId);
 				user.addMatchParticipant(matchParticipant);
 				game.addMatchParticipant(matchParticipant);
 				ParticipantDto[] participants = matchDto.getInfo().getParticipants();
-				for(ParticipantDto participant : participants){
+				List<UserGameStat> userGameStats = new ArrayList<>();
+				for (ParticipantDto participant : participants) {
 					UserGameStat userGameStat = new UserGameStat();
 					userGameStat.setByParticipantDto(participant);
-					game.addGameStat(userGameStat);
-					userGameStatRepository.save(userGameStat);
+					userGameStats.add(userGameStat);
 				}
+				game.setGameStats(userGameStats);
 				gameRepository.save(game);
 			}
 			userRepository.save(user);
-		}catch (Exception e){
+		} catch (Exception e) {
 			System.out.println("에러가 발생!!");
 			return null;
 		}
-		//Todo: 가장 마지막 전적의 날짜를 저장해야만 함. (그 이후부터 조회하기 위해서)
-		//Todo: 만약 100개를 받아오면 해당 마지막을 확인해서 다시 요청 후 재조회 해야만 한다.
-		//Todo: 한번 호출 한적 있다면 가장 마지막 날짜를 기준으로 다시 요청
-
+		//Todo: 언제부터 call 해야하는가?
+		//Todo: 한번 호출 한적 있다면 가장 마지막 날짜를 기준으로 다시 요청, 없다면 처음부터 call 해야만 함.
 		return user.getPuuid();
 	}
 
-
-
+	//matchId 별로 게임 정보를 가져와서 저장(참여자 숫자만큼 저장)
+	// @Transactional(readOnly = false)
+	// public void getMatchInfo(String[] matchHistory, User user) {
+	//
+	// }
 
 	@Transactional
 	public UserProfileDto getUserProfileByName(String name) {
@@ -96,20 +119,25 @@ public class UserService {
 	}
 
 	@Transactional
-	public UserProfileDto getUserProfileByNameTag(String name,String tag) {
-		User user = userRepository.findOneWithNameAndTag(name, tag);
-		if (user == null){
-			String puuid = setUserProfile(name,tag);
-			System.out.println("puuid = " + puuid);;
-			if (puuid == null){;
+	public UserProfileDto getUserProfileByNameTag(String name, String tag) {
+		Optional<User> userOptional = userRepository.findBySummonerNameAndTagLineAllIgnoreCase(name, tag);
+		if (userOptional.isEmpty()) {
+			String puuid = setUserProfile(name, tag);
+			if (puuid == null) {
 				throw new ResponseStatusException(
 					HttpStatus.NOT_FOUND, "summoner not found"
 				);
 			}
-			user = userRepository.findOneWithRank(puuid);
+			System.out.println("ERROR :::: puuid = " + puuid);
+			userOptional = userRepository.findById(puuid);
 		}
 		UserProfileDto userProfileDto = new UserProfileDto();
-		userProfileDto.setByUser(user,requestUrl);
+		if (userOptional.isEmpty()) {
+			throw new ResponseStatusException(
+				HttpStatus.NOT_FOUND, "summoner not found"
+			);
+		}
+		userProfileDto.setByUser(userOptional.get(), requestUrl);
 		return userProfileDto;
 	}
 }
