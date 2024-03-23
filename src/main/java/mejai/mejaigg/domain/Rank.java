69434b2e8d1b7f0e04c36789f8e0a734b41cd43c
hawardShin <joushin@student.42seoul.kr>
2024-03-23 16:59:18 +0900

    Feat: Add unRank Set

--- src/main/java/mejai/mejaigg/domain/Rank.java
+++ src/main/java/mejai/mejaigg/domain/Rank.java
@@ -6,24 +6,22 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import lombok.Getter;
-import mejai.mejaigg.common.RomanNumber;
-import mejai.mejaigg.dto.riot.RankDto;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
 
 @Entity
-@Getter
+@Data
 public class Rank {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // ID 자동 생성 전략 사용
 	@Column(name = "rank_id")
 	private Long id; // Rank의 고유 ID
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	@OneToOne(fetch = FetchType.LAZY)
 	private User user; // Rank를 가지고 있는 User
 
 	private String tier; //ex EMERALD
-	private int rank; //ex IV :  String ->INT
+	private String rank; //ex IV :  String ->INT
 	private String tierImage; //해당 티어 이미지를 url로 저장 추후 정규화 필요할지도.
 	private Long leaguePoints;
 	private String leagueId;
@@ -33,25 +31,23 @@ public class Rank {
 	private boolean veteran; //베테랑 여부
 	private boolean freshBlood; //신규 여부
 	private boolean inactive; //휴식 여부
-
+	private String queueType; //RANKED_SOLO_5x5
 	private int season;//시즌 몇 인지
 
 	public void setUser(User user) {
 		this.user = user;
 	}
 
-	public void setRankByRankDto(RankDto rankDto, int season) {
-		this.tier = rankDto.getTier();
-		this.rank = RomanNumber.romanToInt(rankDto.getRank());
-		this.leaguePoints = rankDto.getLeaguePoints();
-		this.wins = rankDto.getWins();
-		this.losses = rankDto.getLosses();
-		this.hotStreak = rankDto.isHotStreak();
-		this.veteran = rankDto.isVeteran();
-		this.freshBlood = rankDto.isFreshBlood();
-		this.inactive = rankDto.isInactive();
-		this.leagueId = rankDto.getLeagueId();
-		this.season = season;
-
+	public void setUnRanked() {
+		this.tier = "UNRANKED";
+		this.rank = "I";
+		this.leaguePoints = 0L;
+		this.wins = 0;
+		this.losses = 0;
+		this.hotStreak = false;
+		this.veteran = false;
+		this.freshBlood = false;
+		this.inactive = false;
 	}
+
 }
