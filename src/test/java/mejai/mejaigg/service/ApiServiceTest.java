6174a6e3440e8df02db048c55506259c9b9027e1
hawardShin <joushin@student.42seoul.kr>
2024-03-25 12:27:10 +0900

    Teat: Api with dateYM

--- src/test/java/mejai/mejaigg/service/ApiServiceTest.java
+++ src/test/java/mejai/mejaigg/service/ApiServiceTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import jakarta.transaction.Transactional;
+import mejai.mejaigg.common.YearMonthToEpochUtil;
 import mejai.mejaigg.dto.riot.AccountDto;
 import mejai.mejaigg.dto.riot.RankDto;
 import mejai.mejaigg.dto.riot.SummonerDto;
@@ -30,6 +31,10 @@ public class ApiServiceTest {
 		String summonerName = "hide on bush";
 		String tag = "kr1";
 		String matchId = "KR_6983879233";
+		String dateYM = "2024-01";
+		Long epochSeconds = YearMonthToEpochUtil.convertToEpochSeconds(dateYM);
+		Long epochSeconds2 = YearMonthToEpochUtil.addMonthEpochSecond(dateYM, 1);
+		// Long epochSeconds2 = YearMonthToEpochUtil.convertToEpochSeconds("2024-01");
 		//when
 		SummonerDto summonerDto = apiService.getSummonerByName(summonerName).block();
 		System.out.println("summonerDto = " + summonerDto);
@@ -39,7 +44,11 @@ public class ApiServiceTest {
 		System.out.println("accoutDto = " + accountDto);
 		Set<RankDto> rankDtos = apiService.getRankBySummonerId(summonerDto.getId()).block();
 		System.out.println("rankDtos = " + rankDtos);
-		String[] matchStore = apiService.getMatchHistoryByPuuid(summonerDto.getPuuid(), 100).block();
+		String[] matchStore = apiService.getMatchHistoryByPuuid(summonerDto.getPuuid(),
+			epochSeconds, epochSeconds2, 0L, 100).block();
+		for (String s : matchStore) {
+			System.out.println("s = " + s);
+		}
 		MatchDto matchDto = apiService.getMatchDtoByMatchId(matchStore[0]).block();
 		System.out.println("matchDto = " + matchDto);
 		//then
