fd55fdbbd112bd489c9c9593b494831f74119154
hawardShin <joushin@student.42seoul.kr>
2024-03-30 16:04:47 +0900

    Style: For Compile just pass code

--- src/main/java/mejai/mejaigg/mapper/RankMapper.java
+++ src/main/java/mejai/mejaigg/mapper/RankMapper.java
@@ -1,17 +1,17 @@
-package mejai.mejaigg.mapper;
-
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
-
-import mejai.mejaigg.domain.Rank;
-import mejai.mejaigg.dto.riot.RankDto;
-
-@Mapper
-public interface RankMapper {
-	RankMapper INSTANCE = Mappers.getMapper(RankMapper.class);
-
-	@Mapping(source = "rankDto.rank", target = "rank")
-	Rank toRankEntity(RankDto rankDto);
-
-}
+// package mejai.mejaigg.mapper;
+//
+// import org.mapstruct.Mapper;
+// import org.mapstruct.Mapping;
+// import org.mapstruct.factory.Mappers;
+//
+// import mejai.mejaigg.domain.Rank;
+// import mejai.mejaigg.dto.riot.RankDto;
+//
+// @Mapper
+// public interface RankMapper {
+// 	RankMapper INSTANCE = Mappers.getMapper(RankMapper.class);
+//
+// 	@Mapping(source = "rankDto.rank", target = "rank", ignore = true)
+// 	Rank toRankEntity(RankDto rankDto);
+//
+// }
