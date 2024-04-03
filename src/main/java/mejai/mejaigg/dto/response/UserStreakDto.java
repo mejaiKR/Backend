f3f054431d0e06acdec716fc92ea62f250229ab2
hawardShin <joushin@student.42seoul.kr>
2024-04-03 19:43:28 +0900

    Feat: Api Streak Only Game Count

--- src/main/java/mejai/mejaigg/dto/response/UserStreakDto.java
+++ src/main/java/mejai/mejaigg/dto/response/UserStreakDto.java
@@ -9,17 +9,17 @@ import mejai.mejaigg.domain.MatchDateStreak;
 public class UserStreakDto {
 	private String date;
 	private int gameCount;
-	private int rankCount;
+	// private int rankCount;
 
 	public void setDummy(int year, int month, int day) {
 		this.date = year + "-" + month + "-" + day;
 		this.gameCount = 10;
-		this.rankCount = 5;
+		// this.rankCount = 5;
 	}
 
 	public void setByMatchDateStreak(MatchDateStreak matchDateStreak) {
-		this.date = matchDateStreak.getDate().toLocalDate().format(DateTimeFormatter.ofPattern("yyyy-MM"));
+		this.date = matchDateStreak.getDate().toLocalDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 		this.gameCount = matchDateStreak.getMatches().size();
-		this.rankCount = matchDateStreak.getMatches().size();
+		// this.rankCount = matchDateStreak.getMatches().size();
 	}
 }
