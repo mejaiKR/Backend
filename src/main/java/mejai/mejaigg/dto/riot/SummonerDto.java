34e631ca896de4928ca0f9652f559f67612fe6d3
hawardShin <joushin@student.42seoul.kr>
2024-04-11 21:11:20 +0900

    Fix: Duplicate 500Key Error - 유저 이름이 제대로 저장 안되는 현상 해결

--- src/main/java/mejai/mejaigg/dto/riot/SummonerDto.java
+++ src/main/java/mejai/mejaigg/dto/riot/SummonerDto.java
@@ -7,7 +7,6 @@ public class SummonerDto {
 	private String accountId;
 	private int profileIconId;
 	private Long revisionDate;
-	private String name;
 	private String id;
 	private String puuid;
 	private Long summonerLevel;
