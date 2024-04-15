0c29604696d088785953d4596bb86d79829c7884
hawardShin <joushin@student.42seoul.kr>
2024-04-15 17:04:00 +0900

    Fix: 25 스택 이상인 경우 25 스택으로 통일

--- src/main/java/mejai/mejaigg/dto/response/UserStreakDto.java
+++ src/main/java/mejai/mejaigg/dto/response/UserStreakDto.java
@@ -21,7 +21,10 @@ public class UserStreakDto {
 	public void setByMatchDateStreak(MatchDateStreak matchDateStreak, String resourceUrl) {
 		this.date = matchDateStreak.getDate().toLocalDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 		this.gameCount = matchDateStreak.getMatches().size();
-		this.imageUrl = resourceUrl + "mejaiStack/" + gameCount + ".svg";
-		// this.rankCount = matchDateStreak.getMatches().size();
+		if (gameCount > 25) {
+			this.imageUrl = resourceUrl + "mejaiStack/25.svg";
+		} else {
+			this.imageUrl = resourceUrl + "mejaiStack/" + gameCount + ".svg";
+		}
 	}
 }
