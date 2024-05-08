d931b37cfde4401b0aa685d9f8f62a21858831c3
hawardShin <joushin@student.42seoul.kr>
2024-05-08 21:03:23 +0900

    Docs: Set Swagger

--- src/main/java/mejai/mejaigg/user/controller/UserController.java
+++ src/main/java/mejai/mejaigg/user/controller/UserController.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import mejai.mejaigg.common.exception.ClientErrorCode;
+import mejai.mejaigg.common.exception.ClientException;
 import mejai.mejaigg.user.dto.request.UserProfileRequest;
 import mejai.mejaigg.user.dto.request.UserStreakRequest;
 import mejai.mejaigg.user.dto.response.UserProfileDto;
@@ -20,35 +24,34 @@ import mejai.mejaigg.user.service.UserStreakService;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000, https://mejai.gg")
+@Tag(name = "Users", description = "소환사 정보 및 게임 통계 API")
 public class UserController {
 
 	private final UserService userService;
 	private final UserStreakService userStreakService;
 
 	@GetMapping("/users/profile")
+	@Operation(summary = "소환사 정보 조회", description = "주어진 소환사 ID로 프로필 정보를 조회합니다.")
 	public UserProfileDto profile(UserProfileRequest request) {
 		return userService.getUserProfileByNameTag(request.getId(), request.getTag());
 	}
 
 	@GetMapping("/users/streak")
-	public List<UserStreakDto> streak(@Valid UserStreakRequest request) {
-		String puuid = userService.getPuuidByNameTag(request.getId(), request.getTag());
-		Optional<List<UserStreakDto>> userMonthStreak;
+	@Operation(summary = "소환사 게임 횟수 및 승패 조회", description = "소환사가 특정 기간 동안 진행한 게임 횟수 및 승패를 조회합니다.")
+	public List<UserStreakDto> streak( @Valid UserStreakRequest request) {
+
 		try {
-			userMonthStreak = userStreakService.getUserMonthStreak(puuid, request.getYear(),
-				request.getMonth());
-			if (userMonthStreak.isEmpty()) {
-				throw new ResponseStatusException(HttpStatus.TOO_MANY_REQUESTS,
-					"Too many requests. Please try again later.");
+			Optional<List<UserStreakDto>> userMonthStreak = userStreakService.getUserMonthStreak(request);
+			return userMonthStreak.get();
+		} catch (ClientException e) {
+			if (e.getClientErrorCode() == ClientErrorCode.INTERNAL_SERVER_ERROR){
+				throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"Roit 서버 에러");
 			}
-		} catch (Exception e) {
-			if (e instanceof ResponseStatusException) {
-				throw e;
+			else if (e.getClientErrorCode() == ClientErrorCode.TOO_MANY_REQUESTS) {
+				throw new ResponseStatusException(HttpStatus.TOO_MANY_REQUESTS, "Too many requests. Please try again later.");
 			}
-			throw new ResponseStatusException(HttpStatus.TOO_MANY_REQUESTS,
-				"Too many requests. Please try again later.");
 		}
-		return userMonthStreak.get();
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "소환사를 찾을 수 없습니다.");
 	}
 }
