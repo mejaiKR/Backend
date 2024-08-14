6c6280ac3dfb1c4427e6628bc4faec8c99c2341b
hawardShin <joushin@student.42seoul.kr>
2024-08-14 14:39:32 +0900

    Chore: Delete MatchRepository.java

--- src/main/java/mejai/mejaigg/match/repository/MatchRepository.java
+++
@@ -1,8 +0,0 @@
-package mejai.mejaigg.match.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import mejai.mejaigg.match.entity.Match;
-
-public interface MatchRepository extends JpaRepository<Match, String> {
-}
