5bd6839aca45422b448164604209c6a82e802106
hawardShin <joushin@student.42seoul.kr>
2024-09-24 09:45:40 +0900

    Feat: Set Sqs acknowledgementMode

--- src/main/java/mejai/mejaigg/messaging/sqs/config/SqsListenerConfig.java
+++ src/main/java/mejai/mejaigg/messaging/sqs/config/SqsListenerConfig.java
@@ -7,12 +7,15 @@ import org.springframework.context.annotation.Configuration;
 
 import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
 import io.awspring.cloud.sqs.listener.SqsMessageListenerContainer;
+import io.awspring.cloud.sqs.listener.acknowledgement.handler.AcknowledgementMode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import mejai.mejaigg.messaging.sqs.listener.MyMessageListener;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class SqsListenerConfig {
 	private final MyMessageListener myMessageListener;
 
@@ -21,14 +24,21 @@ public class SqsListenerConfig {
 		SqsMessageListenerContainerFactory<Object> factory = SqsMessageListenerContainerFactory.builder()
 			.sqsAsyncClient(sqsAsyncClient)
 			.configure(options -> options
-				.maxConcurrentMessages(10)
-				.pollTimeout(Duration.ofSeconds(10)))
+				.acknowledgementMode(AcknowledgementMode.MANUAL) // 수동 확인 모드
+				.maxConcurrentMessages(10) // 최대 동시 메시지 처리 수
+				.pollTimeout(Duration.ofSeconds(10))) // 폴링 타임아웃
 			.build();
 		// SqsMessageListenerContainer 생성
 		SqsMessageListenerContainer<Object> container = factory.createContainer("mejai-renewal-sqs");
 
 		// MessageListener 설정
-		container.setMessageListener(myMessageListener);
+		container.setMessageListener((message) -> {
+			try {
+				myMessageListener.onMessage(message); // 메시지 처리 명시적으로 메시지 삭제 진행
+			} catch (Exception e) {
+				log.warn("Failed to process message: " + e.getMessage());
+			}
+		});
 		return container;
 	}
 
