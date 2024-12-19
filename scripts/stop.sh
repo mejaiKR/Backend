97bddc783eef5c4db0d16ab4f2a3b2b061ec8a94
kimchijinju <hanbimkim@gmail.com>
2024-12-19 17:01:09 +0900

    chore: stop 빌드 스크립트 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -13,7 +13,21 @@ CURRENT_PID=$(pgrep -f $JAR_FILE)
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
-else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -15 $CURRENT_PID
+  exit 0
 fi
+
+echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 시도" >> $DEPLOY_LOG
+kill -15 $CURRENT_PID
+
+TIMEOUT=30
+for i in $(seq 1 $TIMEOUT); do
+    if ! pgrep -f "$JAR_FILE"; then
+        echo "$TIME_NOW > $CURRENT_PID 애플리케이션 종료 완료" >> "$DEPLOY_LOG"
+        exit 0
+    fi
+    echo "$TIME_NOW > 종료 대기 중... ($i/$TIMEOUT)" >> "$DEPLOY_LOG"
+    sleep 1
+done
+
+# 30초내로 종료 실패하면 에러
+exit 1
