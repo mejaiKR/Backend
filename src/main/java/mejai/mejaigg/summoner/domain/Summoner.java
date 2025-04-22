5ce8631b03d54e8de56d2f0e664410d0cda964dd
hawardShin <joushin@student.42seoul.kr>
2025-04-22 20:59:55 +0900

    Revert "Fix: 데이터 삽입 중복키 오류"
    This reverts commit 51e331ff44e2a9c158dcc2b3620d3c15f7a6452d.

--- src/main/java/mejai/mejaigg/summoner/domain/Summoner.java
+++ src/main/java/mejai/mejaigg/summoner/domain/Summoner.java
@@ -15,7 +15,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -38,12 +37,7 @@ import mejai.mejaigg.searchhistory.domain.SearchHistory;
 public class Summoner extends BaseEntity {
 	@Id
 	@Column(name = "id")
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "summoner_seq")
-	@SequenceGenerator(
-		name = "summoner_seq",
-		sequenceName = "summoner_id_seq",
-		allocationSize = 1
-	)
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // ID 자동 생성 전략 사용
 	private Long id;
 
 	@Column(name = "summoner_name", nullable = false)
