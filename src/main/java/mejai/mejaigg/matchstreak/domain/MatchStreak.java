11cbf1b4df0a6baa6ea3ac86a93a144e0a73b380
hawardShin <joushin@student.42seoul.kr>
2025-04-22 21:25:43 +0900

    Fix: Roll back

--- src/main/java/mejai/mejaigg/matchstreak/domain/MatchStreak.java
+++ src/main/java/mejai/mejaigg/matchstreak/domain/MatchStreak.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -29,7 +28,7 @@ import mejai.mejaigg.searchhistory.domain.SearchHistory;
 public class MatchStreak extends BaseEntity implements Comparable<MatchStreak> {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue
 	private Long id;
 
 	@Column(name = "date", nullable = false)
--- src/main/java/mejai/mejaigg/searchhistory/domain/SearchHistory.java
+++ src/main/java/mejai/mejaigg/searchhistory/domain/SearchHistory.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -35,7 +34,7 @@ import mejai.mejaigg.summoner.domain.Summoner;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SearchHistory extends BaseEntity {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue
 	@Column(name = "id")
 	private Long id;
 
