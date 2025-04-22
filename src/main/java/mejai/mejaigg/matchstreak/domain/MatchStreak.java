fbef94c026bb2b10408485e1993454c8898af54b
hawardShin <joushin@student.42seoul.kr>
2025-04-22 21:11:27 +0900

    Revert "Fix: 데이터 삽입 중복키 오류"
    This reverts commit 4c07d9fadbf6a7ca9dfef4d74e8c1055165a3d49.

--- src/main/java/mejai/mejaigg/matchstreak/domain/MatchStreak.java
+++ src/main/java/mejai/mejaigg/matchstreak/domain/MatchStreak.java
@@ -6,11 +6,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -30,12 +28,7 @@ import mejai.mejaigg.searchhistory.domain.SearchHistory;
 public class MatchStreak extends BaseEntity implements Comparable<MatchStreak> {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ms_seq")
-	@SequenceGenerator(
-		name = "ms_seq",
-		sequenceName = "match_streak_seq",
-		allocationSize = 50
-	)
+	@GeneratedValue // ID 자동 생성 전략 사용
 	private Long id;
 
 	@Column(name = "date", nullable = false)
